acd9689f331266b8c530dcedf034be17f00991c8
FlorenciaParedes <flomic08@gmail.com>
2023-12-20 19:00:13 -0300

    El listar gastos de un grupo

--- src/app/component/gastos-por-grupos/gastos-por-grupos.component.ts
+++ src/app/component/gastos-por-grupos/gastos-por-grupos.component.ts
@@ -2,6 +2,7 @@
 import { Component } from '@angular/core';
 import { Grupo } from '../../models/grupo.model';
 import { GruposService } from '../../services/grupos.service'; 
+import { Gasto } from '../../models/gasto.model';
 @Component({
   selector: 'app-gastos-por-grupos',
   templateUrl: './gastos-por-grupos.component.html',
@@ -10,15 +11,14 @@ import { GruposService } from '../../services/grupos.service';
 export class GastosPorGruposComponent {
   grupos: Grupo[] = [];
   grupoSeleccionado: Grupo| null = null;
+  gastos: Gasto[] = [];
+
   constructor(private grupoService: GruposService) {} // Inyecta el servicio en el constructor
   
   ngOnInit() {
-    this.cargarGrupos(); // Llama a la función que realiza la petición en el ngOnInit
+    this.cargarGrupos(); 
   }
 
-  onSubmit(){
-    console.log("estoy harta");
-  }
 
   cargarGrupos() {
     this.grupoService.listarGrupos().subscribe(
@@ -29,6 +29,25 @@ export class GastosPorGruposComponent {
         console.error('Error al cargar los grupos', error);
       }
     );
+    }
+    
+    onSubmit() {
+    if (this.grupoSeleccionado) {
+      console.log(this.grupoSeleccionado.id);
+      this.cargarGastos(this.grupoSeleccionado.id);
+    }
+  }
 
+  cargarGastos(idGrupo: number) {
+    console.log("dentro de cargar gasto",idGrupo);
+    this.grupoService.obtenerGastos(idGrupo).subscribe(
+      (gastos: Gasto[]) => {
+        this.gastos = gastos;
+        console.log('Gastos obtenidos:', this.gastos);
+      },
+      (error) => {
+        console.error('Error al cargar los gastos', error);
+      }
+    );
     }
   }
